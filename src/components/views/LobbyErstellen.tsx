68d8190e0d78c5d6f3da919256dd6540d98719a9
n adm <111ssope@gmail.com>
2024-04-03 23:13:25 +0200

    Witer mit em Kosch am design gschaffed

--- src/components/views/LobbyErstellen.tsx
+++ src/components/views/LobbyErstellen.tsx
@@ -5,16 +5,40 @@ import {useNavigate} from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/Login.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import FormField from "../ui/FormField";
-import FormFieldPassword from "../ui/FormFieldPassword";
+import PropTypes from "prop-types";
 
+/*
+It is possible to add multiple components inside a single file,
+however be sure not to clutter your files with an endless amount!
+As a rule of thumb, use one file per component and only add small,
+specific components that belong to the main one in the same file.
+ */
+const FormField = (props) => {
+  return (
+    <div className="login field">
+      <label className="login label">{props.label}</label>
+      <input type={props.label === "Password" ? "password" : ""}
+             className="login input"
+             placeholder="enter here.."
+             value={props.value}
+             onChange={(e) => props.onChange(e.target.value)}
+      />
+    </div>
+  );
+};
 
-const Login = () => {
+FormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+const LobbyErstellen = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState<string>(null);
   const [password, setPassword] = useState<string>(null);
 
-  
+
   const doLogin = async () => {
     try {
       const requestBody = JSON.stringify({ username, password });
@@ -28,8 +52,8 @@ const Login = () => {
       localStorage.setItem("user_id", user.id);
       localStorage.setItem("token", token);
       navigate("/game");
-      
-      
+
+
     } catch (error) {
       alert(
         "Something went wrong during the login: \n"+handleError(error)
@@ -45,46 +69,30 @@ const Login = () => {
     <div className="basescreen title-screen">
       <div className="basescreen overlay"></div>
       <BaseContainer>
-        <div className="login container">
-          <div className="login form">
-            <h3> Login </h3>
-            <FormField
-              label="Username"
-              value={username}
-              onChange={(un: string) => setUsername(un)}
-            />
-            <FormFieldPassword
-              label="Password"
-              value={password}
-              onChange={(n) => setPassword(n)}
-            />
-            <div className="login button-container">
-              <Button
-                disabled={!username || !password}
-                width="100%"
-                onClick={() => doLogin()}
-              >
-                Login
-              </Button>
-            </div>
-            <div className="login button-container">
-              <Button
-                width="100%"
-                onClick={() => gotoRegistration()}
-              >
-                Register
-              </Button>
-            </div>
+        <div className="basescreen form">
+          <div className="basescreen title">Risk: Global Domination</div>
+          <div className="basescreen buttons-container" style={{gap: '30px'}}>
+            <Button
+              width="100%"
+              onClick ={() => navigate("/join")}>
+              Join Lobby
+            </Button>
+            <Button
+              width="100%">
+              Crate Lobby
+            </Button>
           </div>
         </div>
       </BaseContainer>
     </div>
   );
-
-
 };
 
 /**
  * You can get access to the history object's properties via the useLocation, useNavigate, useParams, ... hooks.
  */
-export default Login;
+export default LobbyErstellen;
+
+
+//for more of the function beside the layout (which will change) I need the backend.
+// We worked on some design ideas in the meantime
