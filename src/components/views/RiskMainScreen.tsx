259a9def61749e9f95efff4a255b44bb0dceeecf
n adm <111ssope@gmail.com>
2024-04-17 13:30:26 +0200

    Added Avatars to the Game Screen

--- src/components/views/RiskMainScreen.tsx
+++ src/components/views/RiskMainScreen.tsx
@@ -1,17 +1,78 @@
-import React from 'react';
+import React, { useState } from "react";
 //import React, { useEffect } from 'react';
 import "styles/views/GameScreen.scss";
+import { api, handleError } from "helpers/api";
 import {Button} from "../ui/Button";
 import {RoundButton} from "../ui/RoundButton";
 import BaseContainer from "../ui/BaseContainer";
+import { useNavigate } from "react-router-dom";
 
 const TitleScreen: React.FC = () => {
     const redButtonRef = React.useRef<HTMLButtonElement>(null);
+    const navigate = useNavigate();
+    const styles = ["Buddy", "Tinkerbell", "leo", "kiki", "Loki", "Gizmo", "Cali", "Missy", "Sasha", "Rascal", "Nala", "Max", "Harley", "Dusty", "Smokey", "Chester", "Callie", "Oliver", "Snicker"];
+    const [num, setNum] = useState(0);
+    const [gesamt, setGesamt] = useState(`https://api.dicebear.com/8.x/thumbs/svg?seed=${styles[num]}`);
+    const id = localStorage.getItem("user_id")
+    let avatarId
+    const [anzeige, setAnzeige] = useState(`https://api.dicebear.com/8.x/thumbs/svg?seed=${styles[num]}`);
     //const redButton = document.getElementById('redButton');
+    const containerStyle: React.CSSProperties = {
+        position: 'fixed',
+        bottom: '10px',
+        right: '0',
+    };
 
+    const imagePairStyle: React.CSSProperties = {
+        display: 'flex',
+    };
+
+    const avatarStylePlaying: React.CSSProperties = {
+        width: '75px', // Adjust the size of the circle
+        height: '75px', // Adjust the size of the circle
+        borderRadius: '50%', // Makes the image circular
+        overflow: 'hidden', // Hides the overflow
+        marginRight: '10px', // Adjust the space between images
+        border: '4px solid red', // Red outline
+    };
+
+    const avatarStyle: React.CSSProperties = {
+        width: '75px', // Adjust the size of the circle
+        height: '75px', // Adjust the size of the circle
+        borderRadius: '50%', // Makes the image circular
+        overflow: 'hidden', // Hides the overflow
+        marginRight: '10px', // Adjust the space between image
+    };
+
+    const imageStyle: React.CSSProperties = {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover', // Ensures the image covers the entire circle
+    };
+    const AvatarCreation = () => {
+        let newNum = num + 1;
+        if (newNum >= 4) {
+            newNum = 0;
+        }
+        setNum(newNum);
+        console.log(newNum)
+        setGesamt(`https://api.dicebear.com/8.x/thumbs/svg?seed=${styles[newNum]}`);
+    }
 
     const nextSate = document.getElementById('nextState');
     React.useEffect(() => {
+        async function gettheUser(id) {
+            try {
+                const config = {Authorization: localStorage.getItem("token"), User_ID: localStorage.getItem("user_id") };
+                const response = await api.get("/users/"+id, {headers: config});
+                avatarId = response.data.avatarId
+                setAnzeige(`https://api.dicebear.com/8.x/thumbs/svg?seed=${styles[avatarId]}`);
+            } catch (error) {
+                alert(
+                  `Something went wrong with fetching the user data: \n${handleError(error)}`
+                );
+            }
+        };
         // Canvas setup
         const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
         const ctx = canvas.getContext('2d');
@@ -110,16 +171,67 @@ const TitleScreen: React.FC = () => {
             </div>
             <div className="gamescreen-innerlower-container">
                 <button
-                    id="nextState"
-                    className="gamescreen-buttons-container"
-                    style={{
-                        left: '5%',
-                        top: '25%',
-                        backgroundColor: 'red',
-                    }}
+                  id="nextState"
+                  className="gamescreen-buttons-container"
+                  style={{
+                      left: '5%',
+                      top: '25%',
+                      backgroundColor: 'red',
+                  }}
                 >
                     Next State
                 </button>
+                <div className="basescreen title-screen">
+                    <div className="basescreen overlay"></div>
+                    <BaseContainer>
+                        <div className="basescreen form">
+                            <button
+                              onClick={() => AvatarCreation()}
+                            > Count
+                            </button>
+                            <div style={containerStyle}>
+                                <div style={imagePairStyle}>
+                                    {num !== 0 ? (
+                                      <div style={avatarStyle}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    ) : (
+                                      <div style={avatarStylePlaying}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    )}
+                                    {num !== 1 ? (
+                                      <div style={avatarStyle}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    ) : (
+                                      <div style={avatarStylePlaying}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    )}
+                                    {num !== 2 ? (
+                                      <div style={avatarStyle}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    ) : (
+                                      <div style={avatarStylePlaying}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    )}
+                                    {num !== 3 ? (
+                                      <div style={avatarStyle}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    ) : (
+                                      <div style={avatarStylePlaying}>
+                                          <img src={anzeige} alt="avatar" style={imageStyle} />
+                                      </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </BaseContainer>
+                </div>
             </div>
         </div>
     );
