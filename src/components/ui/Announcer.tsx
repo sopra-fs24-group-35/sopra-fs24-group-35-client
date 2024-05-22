dabf393d747dda63cf6f17f6cfb7b5a20b7378bd
Burkard Robin <robin.burkard@uzh.ch>
2024-05-22 11:23:25 +0200

    #101: Fixed the colors of the announcer

--- src/components/ui/Announcer.tsx
+++ src/components/ui/Announcer.tsx
@@ -6,49 +6,61 @@ const Announcer = ({ phase, currentPlayerId, userId, game }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [prevPhase, setPrevPhase] = useState(null);
   const [prevPlayerId, setPrevPlayerId] = useState(null);
-  const [initialId, setInitialId] = useState(0);
+  const [currentColor, setCurrentColor] = useState(0);
   const [message, setMessage] = useState("");
   const [count, setCount] = useState(0);
   const intervalRef = useRef(null);
 
   useEffect(() => {
-      // set initial ID
-      if (initialId === null && game?.turnCycle?.currentPlayer?.playerId !== null) {
-        setInitialId(game.turnCycle.currentPlayer.playerId);
-      }
+      // set current color
+      let x = 0;
+        if (game?.turnCycle?.currentPlayer?.playerId) {
+          for (const player of game.players) {
+            if (player.playerId === game.turnCycle.currentPlayer.playerId) {
+              setCurrentColor(x);
+            }
+            x++;
+          }
+        }
       // if it's this player's turn
-      if (prevPhase !== phase && currentPlayerId === userId) {
+      if (prevPhase !== phase || prevPlayerId !== currentPlayerId) {
         setPrevPhase(phase);
         setCount(1)
         // Update the message based on the phase
         let newMessage = "";
-        switch (phase) {
-        case "REINFORCEMENT":
-            newMessage = "Your Turn! Reinforce!";
-            break;
-        case "ATTACK":
-            newMessage = "Time to attack!";
-            break;
-        case "MOVE":
-            newMessage = "Move your troops!";
-            break;
-        default:
-            newMessage = "Phase changed!";
+        if (currentPlayerId === userId) {
+          switch (phase) {
+            case "REINFORCEMENT":
+                newMessage = "Your Turn! Reinforce!";
+                break;
+            case "ATTACK":
+                newMessage = "Time to attack!";
+                break;
+            case "MOVE":
+                newMessage = "Move your troops!";
+                break;
+            default:
+                newMessage = "Phase changed!";
+            }
+        } else if (game?.turnCycle?.currentPlayer?.username) {
+          switch (phase) {
+            case "REINFORCEMENT":
+                newMessage = `${game.turnCycle.currentPlayer.username} reinforces troops!`;
+                break;
+            case "ATTACK":
+                newMessage = `${game.turnCycle.currentPlayer.username} is attacking now!`;
+                break;
+            case "MOVE":
+                newMessage = `${game.turnCycle.currentPlayer.username} may move troops!`;
+                break;
+            default:
+                newMessage = `${game.turnCycle.currentPlayer.username} is on the move!`;
+            }
         }
+        
 
       setMessage(newMessage);
       setIsVisible(true);
-      }
-      // if it's another player's turn
-      else if (prevPlayerId !== currentPlayerId) {
-        setPrevPlayerId(currentPlayerId);
-        setCount(2);
-        let newMessage = "Another player is on the move!";
-        if (game?.turnCycle?.currentPlayer?.username) {
-          newMessage = `${game.turnCycle.currentPlayer.username} is on the move! ${game.turnCycle.currentPlayer.playerId % game.players.length}`;
-        }
-        setMessage(newMessage);
-        setIsVisible(true);
       }
   }, [phase, currentPlayerId, userId, prevPhase]);
 
@@ -79,12 +91,12 @@ const Announcer = ({ phase, currentPlayerId, userId, game }) => {
     <div>
         <div className={`announcerContainer ${isVisible ? "visible" : "hidden"}
           ${game === null ? "c1" : `
-          ${(game.turnCycle.currentPlayer.playerId) % 6 === 0 ? "c1" : `
-          ${(game.turnCycle.currentPlayer.playerId) % 6 === 1 ? "c2" : `
-          ${(game.turnCycle.currentPlayer.playerId) % 6 === 2 ? "c3" : `
-          ${(game.turnCycle.currentPlayer.playerId) % 6 === 3 ? "c4" : `
-          ${(game.turnCycle.currentPlayer.playerId) % 6 === 4 ? "c5" : `
-          ${(game.turnCycle.currentPlayer.playerId) % 6 === 5 ? "c6" : "c1"}`}`}`}`}`}`}`}>
+          ${currentColor === 0 ? "c1" : `
+          ${currentColor === 1 ? "c2" : `
+          ${currentColor === 2 ? "c3" : `
+          ${currentColor === 3 ? "c4" : `
+          ${currentColor === 4 ? "c5" : `
+          ${currentColor === 5 ? "c6" : "c1"}`}`}`}`}`}`}`}>
             <div className={`announcer ${isVisible ? "visible" : "hidden"}`}>
             {message}
             </div>
